b4e9470a8213e9467e6d38a251fe919b7f3ec435
Hayden Ringer <hjrrockies@gmail.com>
2025-03-01 18:07:38 -0500

    adjustments to work with uv

--- setup.py
+++ setup.py
@@ -4,14 +4,13 @@ import numpy as np
 
 define_args = '-DBUILD_WITH_PYTHON3' if sys.version.startswith('3') else ''
 gsvd_extension = Extension(
-                    '_gsvd', 
+                    '_gsvd',
                     ['src/_gsvd.c'],
-                    include_dirs=[
-                            np.get_include(), 
-                            '/usr/local/include'
+                    include_dirs=[np.get_include(),
+                            '/opt/homebrew/Cellar/lapack/3.12.1/include'
                         ],
                     library_dirs=[
-                            '/usr/local/lib'
+                            '/opt/homebrew/Cellar/lapack/3.12.1/lib'
                             ],
                     libraries=['lapacke'],
                     extra_compile_args = [define_args])
@@ -21,29 +20,8 @@ setup(
         version='0.0.2',
         author='Benjamin Naecker',
         author_email='bnaecker@fastmail.com',
-        description='Generalized singular value decomposition of NumPy arrays.',
-        long_description='''
-            The gsvd module is a wrapper for the LAPACK generalized
-            singular value routines for double and complex double arrays.
-            The GSVD is a joint decomposition, which simultaneously diagonalizes
-            a pair of matrices. Intuitively, the decomposition finds a linear
-            subspace which most "overlaps" with the row space of the first
-            matrix and the null space of the second. The decomposition is useful
-            in certain regularization and clustering problems, and can be
-            used to derive solutions to the generalized eigenvalue problem.
-            ''',
-        classifiers = [
-                'Development Status :: Alpha',
-                'Intended Audience :: Science/Research',
-                'Topic :: Scientific/Engineering :: Information Analysis',
-                'Licsense :: OSI Approved :: GNU Public License',
-                'Operating System :: OS Independent',
-                'Programming Language :: Python :: 3'
-            ],
         packages=find_packages(),
         ext_modules=[gsvd_extension],
         install_requires=['numpy>=1.11'],
-        license='GNU Public License v3',
         py_modules=['pygsvd'],
     )
-        
